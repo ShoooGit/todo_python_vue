93fea19d834180084439be619fdf95f71f26442d
ShoooGit <shooo.ity.777@gmail.com>
2020-12-15 15:07:00 +0900

    blueprintを使って機能を分離

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template
-from flask_restful import Api, Resource
+from api import api_bp
 
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
-api = Api(app)
-
-class Spam(Resource):
-  def get(self):
-    return {'id': 42, 'name': 'Name'}
-
-api.add_resource(Spam, '/api/spam')
+app.register_blueprint(api_bp)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
