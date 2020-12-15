6b7a2bfa12960095bad96b07371ea0bc42c80770
ShoooGit <shooo.ity.777@gmail.com>
2020-12-15 16:11:39 +0900

    DBをpostgresqlに変更

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,12 @@ from api import api_bp
 from models import get_all, init_db, insert
 
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
+    'user': 'shuto',
+    'password': 'shuto',
+    'host': '127.0.0.1',
+    'name': 'db.postgresql'
+})
 app.register_blueprint(api_bp)
 
 @app.route('/', defaults={'path': ''})
