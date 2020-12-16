840dfbfee23687aeebf137e07398e8bb26db2b72
ShoooGit <shooo.ity.777@gmail.com>
2020-12-16 16:58:26 +0900

    表示内容の修正

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,8 +3,10 @@
     <h1>This is a home page</h1>
     <el-table class="data-table" :data="tableData" stripe>
       <el-table-column prop="id" label="ID" width="180"></el-table-column>
-      <el-table-column prop="name" label="名前" width="180"></el-table-column>
-      <el-table-column prop="note" label="備考"></el-table-column>
+      <el-table-column prop="name" label="タスク名" width="180"></el-table-column>
+      <el-table-column prop="status" label="状態" width="180"></el-table-column>
+      <el-table-column prop="limit" label="期限" width="180"></el-table-column>
+      <el-table-column prop="detail" label="詳細"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -26,7 +28,7 @@ export default {
   },
   methods: {
     updataTableData: async function () {
-      const response = await axios.get('/api/spam')
+      const response = await axios.get('/api/task')
       this.tableData = response.data
     }
   }
