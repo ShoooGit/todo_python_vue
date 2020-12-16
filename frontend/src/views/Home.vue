17d74a5a4480d32aef13d3a7c7c3a1fcb222069b
ShoooGit <shooo.ity.777@gmail.com>
2020-12-17 00:06:11 +0900

    レイアウトのベースを作成

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="home">
     <h1>Todoリスト</h1>
-    <el-table class="data-table" :data="tableData" stripe>
-      <el-table-column prop="id" label="ID" width="180"></el-table-column>
-      <el-table-column prop="name" label="名前" width="180"></el-table-column>
-      <el-table-column prop="status" label="状態" width="180"></el-table-column>
-      <el-table-column prop="limit" label="期限"></el-table-column>
-    </el-table>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="All" name="all">
+        <el-table class="data-table" :data="tableData" stripe>
+          <el-table-column prop="id" label="ID" width="180"></el-table-column>
+          <el-table-column prop="name" label="名前" width="180"></el-table-column>
+          <el-table-column prop="status" label="状態" width="180"></el-table-column>
+          <el-table-column prop="limit" label="期限"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="Todo" name="todo">
+        <el-table class="data-table" :data="tableData" stripe>
+          <el-table-column prop="id" label="ID" width="180"></el-table-column>
+          <el-table-column prop="name" label="名前" width="180"></el-table-column>
+          <el-table-column prop="status" label="状態" width="180"></el-table-column>
+          <el-table-column prop="limit" label="期限"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="Doing" name="doing">
+        <el-table class="data-table" :data="tableData" stripe>
+          <el-table-column prop="id" label="ID" width="180"></el-table-column>
+          <el-table-column prop="name" label="名前" width="180"></el-table-column>
+          <el-table-column prop="status" label="状態" width="180"></el-table-column>
+          <el-table-column prop="limit" label="期限"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="Done" name="done">
+        <el-table class="data-table" :data="tableData" stripe>
+          <el-table-column prop="id" label="ID" width="180"></el-table-column>
+          <el-table-column prop="name" label="名前" width="180"></el-table-column>
+          <el-table-column prop="status" label="状態" width="180"></el-table-column>
+          <el-table-column prop="limit" label="期限"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
