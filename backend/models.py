6c0d3271599b1461180dda9bca895b25c7c818d8
ShoooGit <shooo.ity.777@gmail.com>
2020-12-16 22:33:47 +0900

    Revert "テーブル定義の変更"
    This reverts commit 31977145ae0d3c11e1c610a6a2e7c618b610ad74.

--- backend/models.py
+++ backend/models.py
@@ -3,14 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Task(db.Model):
-    __tablename__ = 'task'
+class SpamModel(db.Model):
+    __tablename__ = 'spam_table'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    status = db.Column(db.Text, nullable=False)
-    limit = db.Column(db.DateTime, nullable=False)
-    detail = db.Column(db.Text, nullable=True)
+    pk = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    note = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
@@ -19,9 +17,9 @@ def init_db(app):
     db.create_all()
 
 def get_all():
-    return Task.query.order_by(Task.id).all()
+    return SpamModel.query.order_by(SpamModel.pk).all()
 
-def insert(name, status, limit, detail):
-    model = Task(name=name, status=status, limit=limit, detail=detail)
+def insert(name, note):
+    model = SpamModel(name=name, note=note)
     db.session.add(model)
     db.session.commit()
