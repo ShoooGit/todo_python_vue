304d1150becbb4259a14437f0320a01cea762477
ShoooGit <shooo.ity.777@gmail.com>
2020-12-18 00:14:02 +0900

    編集・削除ボタンの追加

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>Todoリスト</h1>
+    <div class="icon">
+      <i class="el-icon-circle-plus"></i>
+    </div>
     <el-tabs class="tab-zone" v-model="activeName">
       <el-tab-pane v-for="tableData in tableDatas" v-bind:key="tableData.tabName" v-bind:label="tableData.tabName" v-bind:name="tableData.tabName">
         <el-table class="data-table" :data=tableData.data stripe>
@@ -8,8 +11,10 @@
           <el-table-column prop="name" label="名前" width="180"></el-table-column>
           <el-table-column prop="status" label="状態" width="180"></el-table-column>
           <el-table-column prop="limit" label="期限"></el-table-column>
-          <el-table-column><i class="el-icon-edit"></i></el-table-column>
-          <el-table-column><i class="el-icon-delete"></i></el-table-column>
+          <el-table-column align="right">
+            <el-button class="el-icon-edit" size="mini" @click="handleEdit()"></el-button>
+            <el-button class="el-icon-delete" size="mini" @click="handleDelete()"></el-button>
+          </el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>
@@ -43,7 +48,14 @@ export default {
         { tabName: 'Doing', data: this.tableData.filter(task => task.status === 'Doing') },
         { tabName: 'Done', data: this.tableData.filter(task => task.status === 'Done') }
       )
+    },
+    handleEdit () {
+      console.log('editボタンを押下')
+    },
+    handleDelete () {
+      console.log('deleteボタンを押下')
     }
+
   }
 }
 </script>
@@ -55,5 +67,14 @@ export default {
 .tab-zone {
   width: 60%;
   margin: auto;
+  margin-top: 40px;
+}
+.icon {
+  width: 60%;
+  margin: auto;
+}
+.el-icon-circle-plus {
+  float: right;
+  color: ＃409EFF;
 }
 </style>
