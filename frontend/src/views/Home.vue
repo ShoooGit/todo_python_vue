18f48082829059907c128b6841e99bf1f1e6a62f
ShoooGit <shooo.ity.777@gmail.com>
2020-12-17 20:08:03 +0900

    冗長な表現の簡略化

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,32 +2,8 @@
   <div class="home">
     <h1>Todoリスト</h1>
     <el-tabs class="tab-zone" v-model="activeName">
-      <el-tab-pane label="All" name="all">
-        <el-table class="data-table" :data="tableDataAll" stripe>
-          <el-table-column prop="id" label="ID" width="180"></el-table-column>
-          <el-table-column prop="name" label="名前" width="180"></el-table-column>
-          <el-table-column prop="status" label="状態" width="180"></el-table-column>
-          <el-table-column prop="limit" label="期限"></el-table-column>
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane label="Todo" name="todo">
-        <el-table class="data-table" :data="tableDataTodo" stripe>
-          <el-table-column prop="id" label="ID" width="180"></el-table-column>
-          <el-table-column prop="name" label="名前" width="180"></el-table-column>
-          <el-table-column prop="status" label="状態" width="180"></el-table-column>
-          <el-table-column prop="limit" label="期限"></el-table-column>
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane label="Doing" name="doing">
-        <el-table class="data-table" :data="tableDataDoing" stripe>
-          <el-table-column prop="id" label="ID" width="180"></el-table-column>
-          <el-table-column prop="name" label="名前" width="180"></el-table-column>
-          <el-table-column prop="status" label="状態" width="180"></el-table-column>
-          <el-table-column prop="limit" label="期限"></el-table-column>
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane label="Done" name="done">
-        <el-table class="data-table" :data="tableDataDone" stripe>
+      <el-tab-pane v-for="tableData in tableDatas" v-bind:key="tableData.tabName" v-bind:label="tableData.tabName" v-bind:name="tableData.tabName">
+        <el-table class="data-table" :data=tableData.data stripe>
           <el-table-column prop="id" label="ID" width="180"></el-table-column>
           <el-table-column prop="name" label="名前" width="180"></el-table-column>
           <el-table-column prop="status" label="状態" width="180"></el-table-column>
@@ -47,11 +23,9 @@ export default {
   name: 'home',
   data () {
     return {
-      tableDataAll: [],
-      tableDataTodo: [],
-      tableDataDoing: [],
-      tableDataDone: [],
-      activeName: 'all'
+      tableData: [],
+      tableDatas: [],
+      activeName: 'All'
     }
   },
   mounted () {
@@ -60,15 +34,12 @@ export default {
   methods: {
     updataTableData: async function () {
       const response = await axios.get('/api/task')
-      this.tableDataAll = response.data
-      this.tableDataTodo = response.data.filter(
-        task => task.status === 'Todo'
-      )
-      this.tableDataDoing = response.data.filter(
-        task => task.status === 'Doing'
-      )
-      this.tableDataDone = response.data.filter(
-        task => task.status === 'Done'
+      this.tableData = response.data
+      this.tableDatas.push(
+        { tabName: 'All', data: this.tableData },
+        { tabName: 'Todo', data: this.tableData.filter(task => task.status === 'Todo') },
+        { tabName: 'Doing', data: this.tableData.filter(task => task.status === 'Doing') },
+        { tabName: 'Done', data: this.tableData.filter(task => task.status === 'Done') }
       )
     }
   }
