0dfb5e6c8cf75104553f142a35199ccbbf8c0764
ShoooGit <shooo.ity.777@gmail.com>
2020-12-18 22:14:12 +0900

    ダイアログの実装

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -20,6 +20,25 @@
         </el-table>
       </el-tab-pane>
     </el-tabs>
+      <el-button type="text" @click="dialogFormVisible = true">open a Form nested Dialog</el-button>
+      <el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+          <el-form-item label="Promotion name" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="Zones" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="Please select a zone">
+              <el-option label="Zone No.1" value="shanghai"></el-option>
+              <el-option label="Zone No.2" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+
   </div>
 </template>
 
@@ -34,7 +53,20 @@ export default {
     return {
       tableData: [],
       tableDatas: [],
-      activeName: 'All'
+      activeName: 'All',
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '120px'
     }
   },
   mounted () {
