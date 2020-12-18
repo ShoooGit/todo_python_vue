70c8d9da6e71f228e6b0c6756b13f814826c08f0
ShoooGit <shooo.ity.777@gmail.com>
2020-12-19 01:29:05 +0900

    入力フォーム（フロント部分）

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="about">
     <!-- Form -->
-    <el-button type="text" @click="dialogFormVisible = true">open a Form nested Dialog</el-button>
-    <el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
+    <el-button class="el-icon-circle-plus" size="mini" @click="dialogFormVisible = true"></el-button>
+    <el-dialog title="タスク登録" :visible.sync="dialogFormVisible">
       <el-form :model="form" class="form">
-        <el-form-item label="Promotion name" :label-width="formLabelWidth">
+        <el-form-item label="件名" :label-width="formLabelWidth">
           <el-input v-model="form.name" autocomplete="off" class="input"></el-input>
         </el-form-item>
-        <el-form-item label="Zones" :label-width="formLabelWidth">
-          <el-select v-model="form.region" placeholder="Please select a zone" class="input">
-            <el-option label="Zone No.1" value="shanghai"></el-option>
-            <el-option label="Zone No.2" value="beijing"></el-option>
+        <el-form-item label="状態" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="タスクの状態を選択して下さい" class="input">
+            <el-option label="Todo" value="Todo"></el-option>
+            <el-option label="Doing" value="Doing"></el-option>
+            <el-option label="Done" value="Done"></el-option>
           </el-select>
+        </el-form-item>
+        <el-form-item label="期限" :label-width="formLabelWidth">
+          <el-date-picker
+            v-model="value"
+            type="date"
+            placeholder="日付を選択して下さい"
+            class="input">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="詳細" :label-width="formLabelWidth">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4}"
+            placeholder="詳細を入力して下さい"
+            v-model="textarea">
+          </el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
+        <el-button @click="dialogFormVisible = false">キャンセル</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">登録</el-button>
       </span>
     </el-dialog>
   </div>
@@ -38,7 +55,34 @@ export default {
         resource: '',
         desc: ''
       },
-      formLabelWidth: '120px'
+      formLabelWidth: '120px',
+      pickerOptions: {
+        disabledDate (time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: 'Today',
+          onClick (picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: 'Yesterday',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: 'A week ago',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
+      },
+      value: '',
+      textarea: ''
     }
   }
 }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,7 +8,7 @@
       <el-tab-pane v-for="tableData in tableDatas" v-bind:key="tableData.tabName" v-bind:label="tableData.tabName" v-bind:name="tableData.tabName">
         <el-table class="data-table" :data=tableData.data stripe>
           <el-table-column prop="id" label="ID" width="180"></el-table-column>
-          <el-table-column prop="name" label="名前" width="180"></el-table-column>
+          <el-table-column prop="name" label="件名" width="180"></el-table-column>
           <el-table-column prop="status" label="状態" width="180"></el-table-column>
           <el-table-column prop="limit" label="期限"></el-table-column>
           <el-table-column align="right" class="el-icon-edit" size="mini">
@@ -20,25 +20,6 @@
         </el-table>
       </el-tab-pane>
     </el-tabs>
-      <el-button type="text" @click="dialogFormVisible = true">open a Form nested Dialog</el-button>
-      <el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
-        <el-form :model="form">
-          <el-form-item label="Promotion name" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="Zones" :label-width="formLabelWidth">
-            <el-select v-model="form.region" placeholder="Please select a zone">
-              <el-option label="Zone No.1" value="shanghai"></el-option>
-              <el-option label="Zone No.2" value="beijing"></el-option>
-            </el-select>
-          </el-form-item>
-        </el-form>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
-        </span>
-      </el-dialog>
-
   </div>
 </template>
 
@@ -53,20 +34,7 @@ export default {
     return {
       tableData: [],
       tableDatas: [],
-      activeName: 'All',
-      dialogTableVisible: false,
-      dialogFormVisible: false,
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      formLabelWidth: '120px'
+      activeName: 'All'
     }
   },
   mounted () {
