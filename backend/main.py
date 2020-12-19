f5641444b53132024fe2a9f85388dca6d283a51f
ShoooGit <shooo.ity.777@gmail.com>
2020-12-19 21:54:18 +0900

    不要な処理を削除

--- backend/main.py
+++ backend/main.py
@@ -20,8 +20,4 @@ if __name__ == '__main__':
   with app.app_context():
     init_db(app)
     # delete()
-    if not get_all():
-      insert('断捨離', 'Todo','2020-12-30', '不要なものを処分する')
-      insert('掃除', 'Doing', '2020-12-20', '部屋の掃除をする')
-      insert('買い物', 'Done', '2020-12-10', '食料品を買う')
   app.run()
