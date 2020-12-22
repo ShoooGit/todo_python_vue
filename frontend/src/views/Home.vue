f74b8bdd52286e473b2498227b1128c77f0e526e
ShoooGit <shooo.ity.777@gmail.com>
2020-12-22 22:30:34 +0900

    フォームをHomeに移動

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="home">
     <h1>Todoリスト</h1>
-    <div class="icon">
-      <i class="el-icon-circle-plus"></i>
-    </div>
+    <el-button class="el-icon-circle-plus" size="mini" @click="dialogFormVisible = true"></el-button>
+    <el-dialog title="タスク登録" :visible.sync="dialogFormVisible">
+      <el-form :model="formData" class="form">
+        <el-form-item label="件名" :label-width="formLabelWidth">
+          <el-input v-model="formData.name" autocomplete="off" class="input"></el-input>
+        </el-form-item>
+        <el-form-item label="状態" :label-width="formLabelWidth">
+          <el-select v-model="formData.status" placeholder="タスクの状態を選択して下さい" class="input">
+            <el-option label="Todo" value="Todo"></el-option>
+            <el-option label="Doing" value="Doing"></el-option>
+            <el-option label="Done" value="Done"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="期限" :label-width="formLabelWidth">
+          <el-date-picker
+            v-model="formData.limit"
+            type="date"
+            placeholder="日付を選択して下さい"
+            class="input"
+            format="yyyy-MM-dd"
+            value-format="yyyy-MM-dd">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="詳細" :label-width="formLabelWidth">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4}"
+            placeholder="詳細を入力して下さい"
+            v-model="formData.detail">
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">キャンセル</el-button>
+        <el-button type="primary" @click="submit">登録</el-button>
+      </span>
+    </el-dialog>
     <el-tabs class="tab-zone" v-model="activeName">
       <el-tab-pane v-for="tableData in tableDatas" v-bind:key="tableData.tabName" v-bind:label="tableData.tabName" v-bind:name="tableData.tabName">
         <el-table class="data-table" :data=tableData.data stripe>
@@ -32,9 +66,43 @@ export default {
   name: 'home',
   data () {
     return {
-      tableData: [],      // 各ステータスのデータ配列
-      tableDatas: [],     // 各ステータスのデータ配列が格納された配列
-      activeName: 'All'
+      tableData: [],
+      tableDatas: [],
+      activeName: 'All',
+      dialogFormVisible: false,
+      formData: {
+        name: '',
+        status: '',
+        limit: '',
+        detail: ''
+      },
+      name: [],
+      formLabelWidth: '120px',
+      pickerOptions: {
+        disabledDate (time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: 'Today',
+          onClick (picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: 'Yesterday',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: 'A week ago',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
+      }
     }
   },
   mounted () {
@@ -98,6 +166,13 @@ export default {
           this.tableDatas[3].data.splice(i, 1)
           break
       }
+    },
+    submit: function () {
+      axios.post('/api/task', this.formData)
+        .then(response => {
+          console.log(response.data)
+        })
+      this.dialogFormVisible = false
     }
   }
 }
